3af196db160642e43415a5777d42233b398010f6
Lim Ding Han <dinghan@Lims-MacBook-Air-2.local>
2023-03-09 22:30:34 +0800

    implemented shortUrl generation

--- index.js
+++ index.js
@@ -4,30 +4,58 @@ const mongoose = require('mongoose');
 const UrlModel = require('./models/UrlLinks');
 
 const cors = require('cors');
+const { exists } = require('./models/UrlLinks');
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    "mongodb+srv://ldinghan:mongoDB@urllinks.0muggcx.mongodb.net/test"
+    "mongodb+srv://ldinghan:mongoDB@UrlLinks.0muggcx.mongodb.net/test"
 );
 
-app.get("/getShortUrl", (req, res) => {
-    UrlModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
+app.get("/getShortUrl", async (req, res) => {
+    const targetUrl = req.query.q;
+    const query = await UrlModel.find({targetUrl}).exec();
+    res.json(query[0].shortUrl);
 });
 
-app.post("/shortenUrl", async (req, res) => {
-    const url = req.body;
-    const newShortUrl = new UrlModel(url);
-    await newShortUrl.save();
+const makeShortUrl = (k) => {
+    str = "";
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < k; i ++) {
+        str += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return str;
+}
 
-    res.json(user);
+
+app.post("/shortenUrl", async (req, res) => {
+    console.log(req.body);
+    const url = req.body;
+    targetUrl = url.targetUrl;
+    existingTargetUrl = await UrlModel.exists({targetUrl});
+    if (existingTargetUrl == null) {
+        counter = 6;
+        str = makeShortUrl(counter);
+        existingShortUrl = await UrlModel.exists({str});
+        while (existingShortUrl != null) {
+            counter++;
+            str = makeShortUrl(counter);
+            existingShortUrl = await UrlModel.exists({str});
+        }
+        
+        
+        newUrlObj = {
+            targetUrl,
+            shortUrl: str
+        }
+
+        const newShortUrl = new UrlModel(newUrlObj);
+        await newShortUrl.save();
+    }
+
+
+    res.json(url);
 })
 
 
